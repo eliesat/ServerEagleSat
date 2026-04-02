ae80a6aefa8bf8a521a5835a99d51a116e5a5cdc
eliesat <112230228+eliesat@users.noreply.github.com>
2026-04-02 07:43:17 +0300

    Add files via upload

---
+++ submenus_list/Eagle1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Screens.Screen import Screen
+from Components.Label import Label
+from Components.ActionMap import ActionMap
+
+
+class Eagle1(Screen):
+
+    def __init__(self, session):
+        Screen.__init__(self, session)
+
+        self.skin = """
+        <screen name="Blank1" position="center,center" size="600,200" title="Blank1">
+            <widget name="text" position="20,60" size="560,80" font="Regular;40" halign="center" />
+        </screen>
+        """
+
+        self["text"] = Label("ServerEagleSat")
+
+        print("ServerEagleSat")
+
+        # -------------------------
+        # EXIT / BACK CONTROL
+        # -------------------------
+        self["actions"] = ActionMap(
+            ["OkCancelActions", "ColorActions", "WizardActions"],
+            {
+                "cancel": self.close,
+                "back": self.close,
+                "ok": self.close
+            },
+            -1
+        )
